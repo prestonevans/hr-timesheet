863d4094c9400369a216ac7c64ea090879b0aae1
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-03-02 10:21:05 -0700

    finished unit 6

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from 'src/app/services/departments.service';
 import { Department } from 'src/app/interfaces/department';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
@@ -10,11 +12,15 @@ export class DepartmentsComponent implements OnInit {
   departments: Department[];
 
   constructor(
-    private departmentsService: DepartmentsService
+    private departmentsService: DepartmentsService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.departments =  this.departmentsService.departments
   }
 
+  goToDepartment(departmentId: string): void {
+    this.router.navigate(['./timesheet', {id: departmentId}]);
+  }
 }
