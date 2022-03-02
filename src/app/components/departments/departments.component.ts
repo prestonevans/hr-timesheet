d6e1915dc280855daf662b1e93e21ca93e6a09ba
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-03-02 09:53:48 -0700

    finished unit 5

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { Department } from 'src/app/interfaces/department';
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
   styleUrls: ['./departments.component.scss']
 })
 export class DepartmentsComponent implements OnInit {
+  departments: Department[];
 
-  constructor() { }
+  constructor(
+    private departmentsService: DepartmentsService
+  ) { }
 
   ngOnInit(): void {
+    this.departments =  this.departmentsService.departments
   }
 
 }
